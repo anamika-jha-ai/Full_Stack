7bae31ec2fcefd99245e0a0d9ddb9144b0371269
Anamika Jha <anamika.jha.cse@gmail.com>
2026-09-12 12:25:19 +0530

    Adding js part 5

---
+++ JavaScript/JavaScript Part5/assignment5.js
@@ -0,0 +1,24 @@
+// question 1(generates random number representing a dice roll)
+let dice = Math.floor(Math.random()* 6)+1;
+console.log(dice);
+
+
+//question 2 (creating a object that stores the car details)
+const car = {
+    name: " BMW ",
+    model: "M5",
+    color :"Black",
+};
+
+console.log(car.name);
+
+//question 3 (creating a object with the details of a person)
+
+const person = {
+    name : "Anmika Jha",
+    age : " 19 ",
+    city : " kolkata , West bengal",
+};
+person.city = "Rishra";
+person.country = "India";
+console.log(person);
